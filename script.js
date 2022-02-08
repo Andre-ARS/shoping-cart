9e95df02ec0beb3ae69bf7b0bdaa4f0e7b9d85a3
André Rodrigues <andre360.ars@gmail.com>
2022-02-08 13:22:44 -0300

    adiciona eventos para esconder o carrinho e a barra de pesquisa, e adiciona animação de spinner no carregamento

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const cart = document.querySelector('.cart__items');
 const changer = ({ id, title, price }) => ({ sku: id, name: title, salePrice: price });
 const searchBtn = document.querySelector('.fa-search');
+const searchBar = document.querySelector('.search-bar');
+const cartIcon = document.querySelector('.cart-icon');
+const closeCartBtn = document.querySelector('.fa-window-close');
 
 function setPrice() {
   let finalPrice = 0;
@@ -25,45 +28,55 @@ function emptyCart() {
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
-  img.src = imageSource;
+  img.src = imageSource.slice(0, -5).concat('J.jpg');
   return img;
 }
 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
-  e.innerText = innerText;
+  e.innerHTML = innerText;
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ sku, name, image, price }) {
   const section = document.createElement('section');
   section.className = 'item';
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createCustomElement('span', 'item__price', `<span class='sifra'>R$</span> <strong>${price.toFixed(2)}</strong>`));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
 }
 
 function loading() {
-  const section = createCustomElement('section', 'loading', 'carregando...');
+  const section = createCustomElement('section', 'loading', '');
+  const loadingText = createCustomElement('span', 'loading-text', 'carregando...');
+  const spinner = createCustomElement('i', 'fas fa-spinner', '')
   
+  section.appendChild(loadingText);
+  section.appendChild(spinner);
   document.querySelector('.items').appendChild(section);
 }
 
 const items = document.querySelector('.items');
-const nameChanger = ({ id, title, thumbnail }) => ({ sku: id, name: title, image: thumbnail });
+const nameChanger = ({ id, title, thumbnail, price }) => ({ sku: id, name: title, image: thumbnail, price });
 
 async function productList(product, callback) {
   const response = await fetchProducts(product);
   const { results } = response;
 
-  results.forEach((result) => {
-    items.appendChild(callback(nameChanger(result)));
-  });
+  if (results.length > 0) {
+    results.forEach((result) => {
+      items.appendChild(callback(nameChanger(result)));
+    });
+  } else {
+    const error = createCustomElement('span', 'error', `O produto '${product}', não foi encontrado! Verifique a ortografia`);
+    items.appendChild(error);    
+  }
 }
 
 async function load(product) {
@@ -113,20 +126,58 @@ function getStoragedItems() {
   cart.addEventListener('click', cartItemClickListener);
 } 
 
-async function search() {
-  const searchBar = document.querySelector('.search-bar');
-  items.innerHTML = '';
+function searchBarHide() {
+  if (searchBar.style.display === '') {    
+    searchBar.style.display = 'none';
+  } else {
+    searchBar.style.display = '';
+  }
+}
 
-  await load(searchBar.value);
-  searchBar.value = '';
+async function search(event) {
+  if (searchBar.value !== '' && event.key === 'Enter') {    
+    items.innerHTML = '';
+
+    await load(searchBar.value);
+    searchBar.value = '';
+  }
+}
+
+function hideCart() {
+  const cartTitle = document.querySelector('.container-cartTitle');
+  const cartSection = document.querySelector('.cart')
+  if (cartTitle.style.visibility === '') {    
+    cartTitle.style.visibility = 'hidden';
+    cartTitle.style.opacity = '0';
+    cartSection.style.visibility = 'hidden';
+    cartSection.style.opacity = '0';
+    closeCartBtn.style.visibility = 'hidden';
+  } else {
+    cartSection.style.visibility = '';
+    cartTitle.style.opacity = '1';
+    cartTitle.style.visibility = '';
+    cartSection.style.opacity = '1';
+    closeCartBtn.style.visibility = '';
+    setPrice();
+  }
+
+}
+
+function events() {
+  searchBtn.addEventListener('click', searchBarHide);
+  searchBar.addEventListener("keyup", search);
+  cartIcon.addEventListener('click', hideCart)
+  closeCartBtn.addEventListener('click', hideCart)
+  document.querySelector('.empty-cart').addEventListener('click', emptyCart);
+  cart.addEventListener('click', cartItemClickListener);
 }
 
 window.onload = () => {
-  searchBtn.addEventListener('click', search)
-  document.querySelector('.empty-cart').addEventListener('click', emptyCart);
   load('computador');
+  searchBarHide();
   getStoragedItems();
-  cart.addEventListener('click', cartItemClickListener);
+  events();
+  hideCart();
   document.querySelector('.items').addEventListener('click', function listener(event) {
     cartList(event, createCartItemElement);
   });
